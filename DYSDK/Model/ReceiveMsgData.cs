68896ecff606931fd296161a0badc7472cdfa793
dengyuwei <835517873@qq.com>
2019-11-09 09:37:39 +0800

    对接私信接收，webhooks回调

---
+++ DYSDK/Model/ReceiveMsgData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DYSDK.Model
+{
+    /// <summary>
+    /// 接收私信消息 目前只对接文本消息
+    /// </summary>
+    public class ReceiveMsgData
+    {
+        [JsonProperty("message_type")]
+        public string MessageType { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ DYSDK/Model/WebhooksData.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DYSDK.Model
+{
+    /// <summary>
+    /// webhooks 消息结构实体
+    /// </summary>
+    public class WebhooksData
+    {
+        public string MsgId { get; set; }
+        /// <summary>
+        /// 时间名称
+        /// </summary>
+        [JsonProperty("event")]
+        public string Event { get; set; }
+
+        /// <summary>
+        /// 用户 openid
+        /// </summary>
+        [JsonProperty("from_user_id")]
+        public string FromUserId { get; set; }
+
+        /// <summary>
+        /// 用户 openid
+        /// </summary>
+        [JsonProperty("to_user_id")]
+        public string ToUserId { get; set; }
+
+        /// <summary>
+        /// 应用 key
+        /// </summary>
+        [JsonProperty("client_key")]
+        public string ClientKey { get; set; }
+
+        /// <summary>
+        /// 消息内容
+        /// </summary>
+        [JsonProperty("content")]
+        public object Content { get; set; }
+    }
+}
